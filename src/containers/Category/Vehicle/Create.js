53ca62c6532744663ae81e1545e2e76602ed2d4d
tuanminhcr7 <tuanminh352000@gmail.com>
2023-05-25 11:28:42 +0700

    fix phuong tien & nhan su

--- src/containers/Category/Vehicle/Create.js
+++ src/containers/Category/Vehicle/Create.js
@@ -11,40 +11,40 @@ const Create = ({
   onHiddenModal,
   onRefreshList,
   itemSelected,
-  products
+  products,
+  seatType
 }) => {
   const [form] = Form.useForm();
   const onFinishFailed = () => {
   };
 
-  
+
 
   const onSave = async (values) => {
-    
+
     const params = {
       ...values,
       insurance_expired_date: moment(values.insurance_expired_date).format('YYYY-MM-DD'),
       registration_expired_date: moment(values.registration_expired_date).format('YYYY-MM-DD'),
-      is_active: values?.is_active ? 1: 0
+      is_active: values?.is_active ? 1 : 0,
+      seat_type: values?.seat_type
     }
-    category.createVehicle(params)
-          .then(res => {
-              if (res.status === 200) {
-                Ui.showSuccess({ message: "Thành công" });
-                onRefreshList()
-                onHiddenModal()
-               
-              }
-          })
-          .catch(err => {
-              if (err.response?.status === 422 && err.response?.data?.errors) {
-                  message.warn(err.response.data?.errors[0].msg)
-                  message.error('Error!')
-              }
-          })
+    category.createVehicle(params).then(res => {
+      if (res.status === 200) {
+        Ui.showSuccess({ message: "Thành công" });
+        onRefreshList();
+        onHiddenModal();
+      }
+    })
+      .catch(err => {
+        if (err.response?.status === 422 && err.response?.data?.errors) {
+          // message.warn(err.response.data?.errors[0].msg)
+          message.error(err?.response?.data?.message);
+        }
+      })
 
   }
-  
+
   return (
     <div className={className}>
       <FormAddEdit
@@ -52,6 +52,7 @@ const Create = ({
         onSave={onSave}
         onHiddenModal={onHiddenModal}
         products={products}
+        seatType={seatType}
       />
     </div>
   );
